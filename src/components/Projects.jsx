4ee74e0786506d0597f71d68c0642fdbee939101
Alison Estephany <alisonestephanyoz@gmail.com>
2022-12-04 15:59:33 -0400

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,23 @@
+const Projects= ()=>{
+  return(
+    <div>
+      <a href="">
+        <img 
+          src={""}
+          alt=""
+        />
+      </a>
+    </div>
+  );
+};
+
+export default Project;
+/*
+<h3>Project One</h3>
+<p class="subtext">This is my project and some text about it. I enjoyed making this project.</p>
+<hr/>
+<p class="subtext"><a class="project-link" href="https://www.github.com/kubowania">View here</a></p>
+</div>
+<div class="project-card">
+<img class="project-image" src="src/images/project2.jpg" alt="Project One Image"/>
+*/
